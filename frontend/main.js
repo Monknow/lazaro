24826aa6061475bd1aa6967fb397667c028cf29a
Juan <juanrodriguezcode@gmail.com>
2024-10-25 12:53:13 -0600

    added initial extension

--- frontend/main.js
+++
@@ -1,8 +0,0 @@
-console.log("The extension is up and running");
-
-var images = document.getElementsByTagName("img");
-
-for (elt of images) {
-	elt.src = `${browser.runtime.getURL("test.jpg")}`;
-	elt.alt = "an alt text";
-}
